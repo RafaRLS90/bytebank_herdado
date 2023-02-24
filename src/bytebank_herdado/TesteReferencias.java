389bb5cc0faaa13abfb53bad427535504cc5d2a5
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-23 23:20:37 -0300

    subindo a classe designer

--- src/bytebank_herdado/TesteReferencias.java
+++ src/bytebank_herdado/TesteReferencias.java
@@ -3,22 +3,27 @@ package bytebank_herdado;
 public class TesteReferencias {
 	public static void main(String[] args) {
 		
-		Gerente g1 = new Gerente();
+				Gerente g1 = new Gerente();
 				g1.setNome("Marcio");
 				g1.setSalario(5000.0);
 				
-		Funcionario f = new Funcionario();
+				Funcionario f = new Funcionario();
 				f.setNome("Alexandre");
 				f.setSalario(2000.0);
 				
-	    EditorDeVideo ev = new EditorDeVideo();
+				EditorDeVideo ev = new EditorDeVideo();
 				ev.setNome("Elton");
 				ev.setSalario(2500.0);
+				
+				Designer d = new Designer();
+				d.setNome("Gabriel");
+				d.setSalario(2000.0);
 				
 				ControleBonificacao controle = new ControleBonificacao();
 				controle.registra(g1);
 				controle.registra(f);
 				controle.registra(ev);
+				controle.registra(d);
 				
 				System.out.println(controle.getSoma());
 	}
